a7313f47697d38c4dbd303fd673ac709633da8f4
shrawankansi <shrawankansi@gmail.com>
2023-07-06 12:08:04 +0530

    Weather app

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,115 @@
+
+'use client'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [weather, setWeather] = useState('')
+  const[location, setLocation] = useState('')
+ 
+  const getWeather = async() =>{
+  const api_key ='c05c9a46370c48a6a0042935230607'
+  const api_url= 'http://api.weatherapi.com/v1/current.json?key='+ api_key+'&q='+location
+
+  if (location) {
+    try {
+      const res = await fetch(api_url)
+      const data = await res.json()
+      if (data) {
+        const api_data = {
+          country: data.location.country,
+          city: data.location.name,
+          temp: data.current.temp_c,
+          humidity: data.current.humidity,
+          wind: data.current.wind_mph,
+          gust: data.current.gust_mph,
+          visibility: data.current.vis_miles,
+          condition: data.current.condition.text,
+          img: data.current.condition.icon
+        }
+        
+        setWeather(<>
+          <div className="text-center text-2xl p-2">{api_data.city}</div>
+          <div className="flex justify-center">
+            <div className="flow-root">
+              <div className="float-left"><img src={api_data.img} width="80" height="80" alt="Condition" /></div>
+              <div className="float-left text-6xl degrees">{api_data.temp}</div>
+            </div>
+          </div>
+          <div className="text-center text-gray-600">{api_data.condition}</div>
+          <div className="flow-root p-2">
+            <div className="float-left text-gray-600">Humidity: {api_data.humidity} %</div>
+            <div className="float-right text-gray-600">Wind: {api_data.wind} <span className="">mph</span></div>
+            <div className="float-left text-gray-600">Visibility: {api_data.visibility} mi</div>
+            <div className="float-right text-gray-600">Gust: {api_data.gust} mph</div>
+          </div>
+        </>)
+
+        setLocation('')
+
+        console.log(data)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  } else {
+    // Invalid: location is empty
+  }
+}
+
+const handleKeyUp = (key) => {
+  if (key === "Enter") {
+    getWeather()
+  }
+}
+
+
+
+
+
+  
+       return (
+    <>
+        <nav className="flex items-center justify-center py-4 bg-gray-100 w-full m-0 opacity-90">
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input className="block bg-slate-700 text-white rounded-lg opacity-70 pl-10 p-2" 
+            type="text" 
+            id="location" 
+            value={location} 
+            onChange={(e) => setLocation(e.target.value)} 
+            onKeyUp={(e) => handleKeyUp(e.key)}
+            placeholder="Location (ie. Delhi)" />
+          </div>
+
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-2 p-2.5 rounded-lg" id="search" onClick={getWeather}>
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            <span className="sr-only">GO</span>
+          </button>
+        </nav>
+
+        { weather &&
+        <div className="flex w-full p-20 justify-center">
+          <div className="w-full max-w-xs">
+            <div className="mb-4">
+              <div className="bg-white shadow-lg rounded-3xl px-8 pt-6 pb-8 mb-4 opacity-80">
+                {weather}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+        }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="flex justify-center p-20 pt-20 mt-20">
+          <div className="flex bg-white rounded-3xl px-4 py-1 color-gray opacity-90 hover:scale-150 transition-all">
+            <img className="codrkai-profile object-cover rounded-full w-16 h-16 m-2" src="https://avatars.githubusercontent.com/shrawankansi" alt="shrawankansi" />
+            <div className="text-sm text-gray-400 pt-3">shrawankansi<br />WeatherApp<br />&copy; 2023</div>
+          </div>
+        </div>
+    </>
   )
+
+  
 }
