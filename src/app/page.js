7c0ecd90acd5a924f7c1ef0e5bc04821df91d8c7
shrawankansi <shrawankansi@gmail.com>
2023-07-07 23:59:59 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,7 @@ const handleKeyUp = (key) => {
 
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-2 p-2.5 rounded-lg" id="search" onClick={getWeather}>
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-            <span className="sr-only">GO</span>
+            <span className="sr-only">Search</span>
           </button>
         </nav>
 
